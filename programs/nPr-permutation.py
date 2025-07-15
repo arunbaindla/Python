6b8864b607d0aed41fd36965e37e07030e32f57e
arunbaindla <arunkumar.888.b@gmail.com>
2025-07-15 10:02:54 +0530

    Create nPr-permutation.py

---
+++ programs/nPr-permutation.py
@@ -0,0 +1,10 @@
+def fact(n):
+    if n==0 or n==1 :
+        return 1 
+    elif(n>1):
+        return n*fact(n-1)
+
+a=int(input("enter n value in nPr"))
+r=int(input("enter r value in nPr"))
+
+print(fact(a)//fact(r))
